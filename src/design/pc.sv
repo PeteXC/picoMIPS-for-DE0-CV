e13a7cb4ad916d272edcbeda994b7535d1e88085
Peter C <peterchai2008@hotmail.co.uk>
2019-04-15 18:05:45 +0100

    Update - updated PC Please note that there is no branching capabilities in this processor, it will only increment when told to

--- src/design/pc.sv
+++ src/design/pc.sv
@@ -5,28 +5,21 @@
 // Author: tjk
 // Last rev. 24 Oct 2012
 //-----------------------------------------------------
-module pc #(parameter Psize = 6) // up to 64 instructions
-	(input logic clk, reset, PCincr,PCabsbranch,PCrelbranch,
-	input logic [Psize-1:0] Branchaddr,
+module pc #(parameter Psize = 5) // up to 64 instructions
+	(input logic clk, reset, PCincr,
 	output logic [Psize-1 : 0]PCout
 	);
 
 	//------------- code starts here---------
-	logic[Psize-1:0] Rbranch; // temp variable for addition operand
-
-	always_comb
-	if (PCincr)	// if we increment, set the temp var to 1
-		Rbranch = { {(Psize-1){1'b0}}, 1'b1};
-	else Rbranch =  Branchaddr; // Else we will set the branch address
-
 
 	always_ff @ ( posedge clk or posedge reset) // async reset
-
-	if (reset) // sync reset
-		PCout <= {Psize{1'b0}};
-	else if (PCincr | PCrelbranch) // increment or relative branch (note increment overrides relative branch)
-		PCout <= PCout + Rbranch; // 1 adder does both
-	else if (PCabsbranch) // absolute branch
-		PCout <= Branchaddr;
+	begin
+		if (reset) // sync reset
+			PCout <= {Psize{1'b0}};
+		else if (PCincr)
+			PCout <= PCout + 1'b1;
+		else
+			PCout = PCout;
+	end
 
 endmodule // module pc
